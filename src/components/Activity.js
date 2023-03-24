3bd60583b62d62408d082329732fc3cd59a662d0
ghshin-a <singahyeon12@naver.com>
2023-03-24 10:09:55 +0900

    complete Spring Acitivites tap

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -28,22 +28,42 @@ const ActivityContent = ({ type, contentList }) => {
   return (
     <div className={`activity-content activity-content-${type}`}>
       <div className="activity-content-up-left">
-        <div className="activity-content-1">{contentList[0]}</div>
+        <div className="activity-content-1">
+          <div className="activity-content-1-image">{contentList[0]}</div>
+        </div>
         <div className="activity-content-up-right">
-          <div className="activity-content-2">{contentList[1]}</div>
-          <div className="activity-content-3">{contentList[2]}</div>
-          <div className="activity-content-4">{contentList[3]}</div>
-          <div className="activity-content-5">{contentList[4]}</div>
+          <div className="activity-content-2">
+            <div className="activity-content-2-image">{contentList[1]}</div>
+          </div>
+          <div className="activity-content-3">
+            <div className="activity-content-3-image">{contentList[2]}</div>
+          </div>
+          <div className="activity-content-4">
+            <div className="activity-content-4-image">{contentList[3]}</div>
+          </div>
+          <div className="activity-content-5">
+            <div className="activity-content-5-image">{contentList[4]}</div>
+          </div>
         </div>
       </div>
       <div className="activity-content-down-left">
         <div className="activity-content-down-right">
-          <div className="activity-content-6">{contentList[5]}</div>
-          <div className="activity-content-7">{contentList[6]}</div>
-          <div className="activity-content-8">{contentList[7]}</div>
-          <div className="activity-content-9">{contentList[8]}</div>
+          <div className="activity-content-6">
+            <div className="activity-content-6-image">{contentList[5]}</div>
+          </div>
+          <div className="activity-content-7">
+            <div className="activity-content-7-image">{contentList[6]}</div>
+          </div>
+          <div className="activity-content-8">
+            <div className="activity-content-8-image">{contentList[7]}</div>
+          </div>
+          <div className="activity-content-9">
+            <div className="activity-content-9-image">{contentList[8]}</div>
+          </div>
         </div>
-        <div className="activity-content-10">{contentList[9]}</div>
+        <div className="activity-content-10">
+          <div className="activity-content-10-image">{contentList[9]}</div>
+        </div>
       </div>
     </div>
   );
