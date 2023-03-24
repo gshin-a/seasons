1eb8d89d19f19129b198c4b0aa23301d7f872050
ghshin-a <singahyeon12@naver.com>
2023-03-24 18:00:09 +0900

    update

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,31 +1,99 @@
-const galleryList = [
+const JejuGalleryList = [
   {
+    id: 1,
     imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-1.jpg`,
     description: "한줄소개1",
   },
   {
+    id: 2,
     imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-2.jpg`,
     description: "한줄소개2",
   },
   {
+    id: 3,
     imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-3.jpg`,
     description: "한줄소개3",
   },
   {
+    id: 4,
     imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-4.jpg`,
     description: "한줄소개4",
   },
   {
+    id: 5,
     imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-5.jpg`,
     description: "한줄소개5",
   },
   {
+    id: 6,
     imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-6.jpg`,
     description: "한줄소개6",
   },
+];
+
+const SeoulGalleryList = [
   {
-    imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-7.jpg`,
-    description: "한줄소개7",
+    id: 1,
+    imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-1.jpg`,
+    description: "한줄소개1",
+  },
+  {
+    id: 2,
+    imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-2.jpg`,
+    description: "한줄소개2",
+  },
+  {
+    id: 3,
+    imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-3.jpg`,
+    description: "한줄소개3",
+  },
+  {
+    id: 4,
+    imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-4.jpg`,
+    description: "한줄소개4",
+  },
+  {
+    id: 5,
+    imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-5.jpg`,
+    description: "한줄소개5",
+  },
+  {
+    id: 6,
+    imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-6.jpg`,
+    description: "한줄소개6",
+  },
+];
+
+const BusanGalleryList = [
+  {
+    id: 1,
+    imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-1.jpg`,
+    description: "한줄소개1",
+  },
+  {
+    id: 2,
+    imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-2.jpg`,
+    description: "한줄소개2",
+  },
+  {
+    id: 3,
+    imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-3.jpg`,
+    description: "한줄소개3",
+  },
+  {
+    id: 4,
+    imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-4.jpg`,
+    description: "한줄소개4",
+  },
+  {
+    id: 5,
+    imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-5.jpg`,
+    description: "한줄소개5",
+  },
+  {
+    id: 6,
+    imgSrc: process.env.PUBLIC_URL + `/assets/gallery-content-6.jpg`,
+    description: "한줄소개6",
   },
 ];
 
@@ -38,35 +106,45 @@ const GalleryItemContent = ({ imgSrc, description }) => {
   );
 };
 
-const GalleryItem = ({ title, type }) => {
+const GalleryItem = ({ title, type, galleryList }) => {
   return (
     <div className="gallery-item">
       <h2>{title}</h2>
       <div className="gallery-item-wrapper">
         <button
-          onClick={() => {
+          className={`gallery-item-left-button gallery-item-left-button-${type}`}
+          onClick={(e) => {
+            document.querySelector(
+              `.gallery-item-right-button-${type}`
+            ).style.visibility = "visible";
+
             const contentWidth = document.querySelector(
               ".gallery-item-content"
             ).offsetWidth;
 
             let currentMargin = document.querySelector(`.gallery-items-${type}`)
               .style.marginLeft;
+            const newMargin =
+              parseInt(contentWidth) + parseInt(currentMargin.slice(0, -2));
 
-            if (currentMargin.length < 1 || currentMargin === "0px") {
-              console.log("이미지가 없습니다.");
-            } else {
-              const newMargin =
-                parseInt(contentWidth) + parseInt(currentMargin.slice(0, -2));
-
-              document.querySelector(
-                `.gallery-items-${type}`
-              ).style.marginLeft = `${newMargin}px`;
-              console.log(
-                "현재 marginLeft: " +
-                  document.querySelector(`.gallery-items-${type}`).style
-                    .marginLeft
-              );
+            document.querySelector(
+              `.gallery-items-${type}`
+            ).style.marginLeft = `${newMargin}px`;
+            if (parseInt(currentMargin.slice(0, -2)) < contentWidth) {
+              e.target.visibility = "hidden";
             }
+
+            currentMargin = document.querySelector(`.gallery-items-${type}`)
+              .style.marginLeft;
+
+            if (parseInt(currentMargin.slice(0, -2)) >= 0) {
+              e.target.style.visibility = "hidden";
+            }
+
+            console.log(currentMargin.slice(0, -2));
+            console.log(parseInt(currentMargin.slice(0, -2)));
+            console.log(contentWidth);
+            console.log(parseInt(currentMargin.slice(0, -2)) < contentWidth);
           }}
         >
           &lt;
@@ -75,6 +153,7 @@ const GalleryItem = ({ title, type }) => {
           <div className={"gallery-items" + ` gallery-items-${type}`}>
             {galleryList.map((it) => (
               <GalleryItemContent
+                key={it.id}
                 imgSrc={it.imgSrc}
                 description={it.description}
               />
@@ -82,7 +161,12 @@ const GalleryItem = ({ title, type }) => {
           </div>
         </div>
         <button
+          className={`gallery-item-right-button-${type}`}
           onClick={(e) => {
+            document.querySelector(
+              `.gallery-item-left-button-${type}`
+            ).style.visibility = "visible";
+
             const contentWidth = document.querySelector(
               ".gallery-item-content"
             ).offsetWidth;
@@ -108,21 +192,14 @@ const GalleryItem = ({ title, type }) => {
                 `.gallery-items-${type}`
               ).style.marginLeft = `${newMargin}px`;
             }
-            console.log(
-              "현재 marginLeft: " +
-                document.querySelector(`.gallery-items-${type}`).style
-                  .marginLeft
-            );
 
             currentMargin = document.querySelector(`.gallery-items-${type}`)
               .style.marginLeft;
-            console.log(totalWidth - Math.abs(currentMargin.slice(0, -2)));
             if (
               totalWidth - Math.abs(currentMargin.slice(0, -2)) - contentWidth <
               visibleWidth
             ) {
-              console.log("이미지 끝");
-              e.target.style.display = "none";
+              e.target.style.visibility = "hidden";
             }
           }}
         >
@@ -136,9 +213,9 @@ const GalleryItem = ({ title, type }) => {
 const Gallery = () => {
   return (
     <div className="gallery">
-      <GalleryItem title="제주도" type={1} />
-      <GalleryItem title="서울" type={2} />
-      <GalleryItem title="부산" type={3} />
+      <GalleryItem title="제주도" type={1} galleryList={JejuGalleryList} />
+      <GalleryItem title="서울" type={2} galleryList={SeoulGalleryList} />
+      <GalleryItem title="부산" type={3} galleryList={BusanGalleryList} />
     </div>
   );
 };
