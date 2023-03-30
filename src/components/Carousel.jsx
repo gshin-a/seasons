2096f9bb437034ba4ed5fefc56a9894cd1ac972b
ghshin-a <singahyeon12@naver.com>
2023-03-30 11:49:27 +0900

    홈화면 이미지 슬라이드 수정

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,53 +1,55 @@
 import { useEffect, useState } from "react";
 
-const CarouselButton = ({ handleCarousel, setAutoslide, autoslide }) => {
+const CarouselButton = ({ handleCarousel, setAutoslide }) => {
   return (
-    <div className={"carousel-button"}>
-      <button
-        className="carousel-button-autoslide"
-        onClick={() => {
-          setAutoslide(!autoslide);
-        }}
-      >
-        {autoslide ? "auto slide off" : "auto slide on"}
-      </button>
-      <button
-        onClick={() => {
-          handleCarousel(0);
-        }}
-      >
-        Spring
-      </button>
-      <button
-        onClick={() => {
-          handleCarousel(1);
-        }}
-      >
-        Summer
-      </button>
-      <button
-        onClick={() => {
-          handleCarousel(2);
-        }}
-      >
-        Autumn
-      </button>
-      <button
-        onClick={() => {
-          handleCarousel(3);
-        }}
-      >
-        Winter
-      </button>
+    <div
+      className={"carousel-button"}
+      onMouseEnter={() => {
+        setAutoslide(false);
+      }}
+      onMouseLeave={() => {
+        setAutoslide(true);
+      }}
+    >
+      <div className="carousel-button-wrapper">
+        <button
+          onClick={() => {
+            handleCarousel(0);
+          }}
+        >
+          Spring
+        </button>
+        <button
+          onClick={() => {
+            handleCarousel(1);
+          }}
+        >
+          Summer
+        </button>
+        <button
+          onClick={() => {
+            handleCarousel(2);
+          }}
+        >
+          Autumn
+        </button>
+        <button
+          onClick={() => {
+            handleCarousel(3);
+          }}
+        >
+          Winter
+        </button>
+      </div>
     </div>
   );
 };
 
-const delay = (res) => {
+const delay = (s) => {
   return new Promise((res) => {
     setTimeout(() => {
       res();
-    }, 2000);
+    }, s * 1000);
   });
 };
 
@@ -56,132 +58,90 @@ const Carousel = ({ isDarkMode }) => {
   const [autoslide, setAutoslide] = useState(true);
 
   const handleCarousel = async (imgNum) => {
-    let newCurImg = imgNum;
+    const imageWrapper = document.querySelector(".carousel-image-wrapper");
+    const carouselButton = document.querySelector(".carousel-button-wrapper");
+    setCurImg(imgNum);
     if (curImg < imgNum) {
-      document.querySelector(".carousel-image-wrapper").style.transition =
-        "margin 2s ease";
-      document.querySelector(".carousel-image-wrapper").style.marginLeft = `-${
-        imgNum * 100
-      }vw`;
-      newCurImg = imgNum;
+      carouselButton.style.pointerEvents = "none";
+      carouselButton.style.opacity = "0.5";
+      imageWrapper.style.transition = "left 2s ease";
+      imageWrapper.style.left = `-${imgNum * 100}vw`;
+
+      await delay(2);
+      carouselButton.style.pointerEvents = "all";
+      carouselButton.style.opacity = "1";
     } else if (curImg > imgNum) {
-      document.querySelector(".carousel-image-wrapper").style.transition =
-        "margin 2s ease";
-      document.querySelector(".carousel-image-wrapper").style.marginLeft = `-${
-        (imgNum + 4) * 100
-      }vw`;
-      newCurImg = imgNum + 4;
+      carouselButton.style.pointerEvents = "none";
+      carouselButton.style.opacity = "0.5";
+
+      imageWrapper.style.transition = "left 2s ease";
+      imageWrapper.style.left = `-${(imgNum + 4) * 100}vw`;
+
+      await delay(2);
+      imageWrapper.style.transition = "none";
+      imageWrapper.style.left = `-${imgNum * 100}vw`;
+
+      carouselButton.style.pointerEvents = "all";
+      carouselButton.style.opacity = "1";
     } else {
       return;
     }
-
-    await delay();
-    if (newCurImg >= 4) {
-      document.querySelector(".carousel-image-wrapper").style.transition =
-        "none";
-      document.querySelector(".carousel-image-wrapper").style.marginLeft = `-${
-        (newCurImg - 4) * 100
-      }vw`;
-      newCurImg = newCurImg - 4;
-    }
-    setCurImg(newCurImg);
   };
 
   const autoHandleCarousel = async (imgNum) => {
+    const imageWrapper = document.querySelector(".carousel-image-wrapper");
     if (imgNum !== 0) {
-      document.querySelector(".carousel-image-wrapper").style.transition =
-        "margin 2s ease";
-      document.querySelector(".carousel-image-wrapper").style.marginLeft = `-${
-        imgNum * 100
-      }vw`;
+      imageWrapper.style.transition = "left 2s ease";
+      imageWrapper.style.left = `-${imgNum * 100}vw`;
     } else {
-      document.querySelector(".carousel-image-wrapper").style.transition =
-        "margin 2s ease";
-      document.querySelector(
-        ".carousel-image-wrapper"
-      ).style.marginLeft = `-400vw`;
-      await delay();
-      document.querySelector(".carousel-image-wrapper").style.transition =
-        "none";
-      document.querySelector(
-        ".carousel-image-wrapper"
-      ).style.marginLeft = `0vw`;
+      imageWrapper.style.transition = "left 2s ease";
+      imageWrapper.style.left = `-400vw`;
+      await delay(2);
+      imageWrapper.style.transition = "none";
+      imageWrapper.style.left = `0vw`;
     }
   };
 
   useEffect(() => {
     if (autoslide) {
-      handleCarousel(0);
-      let i = 1;
+      const carouselButton = document.querySelector(".carousel-button-wrapper");
+
+      carouselButton.style.pointerEvents = "none";
+      carouselButton.style.opacity = 0.5;
+      let i = curImg !== 3 ? curImg + 1 : 0;
       const showCarousel = setInterval(() => {
         autoHandleCarousel(i);
         setCurImg(i);
         if (i === 3) i = 0;
         else i++;
       }, 3000);
-      return () => clearInterval(showCarousel);
+      return () => {
+        clearInterval(showCarousel);
+        carouselButton.style.pointerEvents = "all";
+        carouselButton.style.opacity = "1";
+      };
     }
   }, [autoslide]);
 
   return (
     <div className={"carousel" + (isDarkMode ? " carousel-darkmode" : "")}>
+      {!autoslide && (
+        <div className="carousel-button-autoslide">auto slide off..</div>
+      )}
+      <CarouselButton
+        handleCarousel={handleCarousel}
+        setAutoslide={setAutoslide}
+        autoslide={autoslide}
+      />
       <div className="carousel-image-wrapper">
-        <div className={"carousel-image-spring"}>
-          <CarouselButton
-            handleCarousel={handleCarousel}
-            setAutoslide={setAutoslide}
-            autoslide={autoslide}
-          />
-        </div>
-        <div className={"carousel-image-summer"}>
-          <CarouselButton
-            handleCarousel={handleCarousel}
-            setAutoslide={setAutoslide}
-            autoslide={autoslide}
-          />
-        </div>
-        <div className={"carousel-image-autumn"}>
-          <CarouselButton
-            handleCarousel={handleCarousel}
-            setAutoslide={setAutoslide}
-            autoslide={autoslide}
-          />
-        </div>
-        <div className={"carousel-image-winter"}>
-          <CarouselButton
-            handleCarousel={handleCarousel}
-            setAutoslide={setAutoslide}
-            autoslide={autoslide}
-          />
-        </div>
-        <div className={"carousel-image-fake-spring"}>
-          <CarouselButton
-            handleCarousel={handleCarousel}
-            setAutoslide={setAutoslide}
-            autoslide={autoslide}
-          />
-        </div>
-        <div className={"carousel-image-fake-summer"}>
-          <CarouselButton
-            handleCarousel={handleCarousel}
-            setAutoslide={setAutoslide}
-            autoslide={autoslide}
-          />
-        </div>
-        <div className={"carousel-image-fake-autumn"}>
-          <CarouselButton
-            handleCarousel={handleCarousel}
-            setAutoslide={setAutoslide}
-            autoslide={autoslide}
-          />
-        </div>
-        <div className={"carousel-image-fake-winter"}>
-          <CarouselButton
-            handleCarousel={handleCarousel}
-            setAutoslide={setAutoslide}
-            autoslide={autoslide}
-          />
-        </div>
+        <div className={"carousel-image-spring"}></div>
+        <div className={"carousel-image-summer"}></div>
+        <div className={"carousel-image-autumn"}></div>
+        <div className={"carousel-image-winter"}></div>
+        <div className={"carousel-image-fake-spring"}></div>
+        <div className={"carousel-image-fake-summer"}></div>
+        <div className={"carousel-image-fake-autumn"}></div>
+        <div className={"carousel-image-fake-winter"}></div>
       </div>
     </div>
   );
